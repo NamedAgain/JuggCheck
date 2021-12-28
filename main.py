c79a04634e6df30a1184c4f2bd28e754bf4725e5
NamedAgain <greysequiv@gmail.com>
2021-12-28 18:34:32 -0500

    Co-authored-by: Dave Bhardwaj <davebhardwaj@gmail.com>

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import discord
-print('Hello')
+import os
+
+client = discord.Client()
+
+@client.event
+async def ready_on():
+    print('{0.user}.format(client)) has logged in')
+
+@client.event
+async def jc(message):
+    if message.author==client.user:
+        return
+
+    if message.content.startswith('$'):
+        await message.channel.send('Check your juggs')
+
+client.run(os.getenv('TOKEN'))            
