bd9e4ba4bbdbe21a767e3840f9550c0d1b3f5859
NamedAgain <greysequiv@gmail.com>
2021-12-28 19:09:54 -0500

    works

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import discord
 import os
 
-client = discord.Client()
+# client = discord.Client()
 
-@client.event
-async def ready_on():
-    print('{0.user}.format(client)) has logged in')
+# @client.event
+# async def ready_on():
+#     print('{0.user}.format(client)) has logged in')
 
-@client.event
-async def jc(message):
-    if message.author==client.user:
-        return
+# @client.event
+# async def jc(message):
+#     if message.author==client.user:
+#         return
 
-    if message.content.startswith('$'):
-        await message.channel.send('Check your juggs')
+#     if message.content.startswith('.'):
+#         await message.channel.send('Check your juggs')
 
-client.run(os.getenv('TOKEN'))            
+# client.run('ODk3OTg0Nzc2MDA1MzUzNTUy.YWdnfg.Ks_Fl2tuFTiM4_I0TBw7qaFUNHw')            
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply('Check your juggs')
+bot.run('ODk3OTg0Nzc2MDA1MzUzNTUy.YWdnfg.Ks_Fl2tuFTiM4_I0TBw7qaFUNHw')            
